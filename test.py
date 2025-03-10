d1c468977e2e021496211975bf9eeb498c818447
DemigodKS <mankok222@gmail.com>
2025-03-10 23:36:10 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,54 +1,74 @@
-# ссылка на задачу https://www.codewars.com/kata/53fc954904a45eda6b00097f/python 
-
 #6kyu
+
+#method chaining
 class Filter:
     def __init__(self, total: int):
         self.list_of_numbers = []
         self.total = total
 
+
     def add_numbers(self) -> list:
-
         for i in range(1, self.total+1):
-            n = int(input('Введите число от 1 до 50: '))
-            if n > 50:
-               print(f'{n} не в рамках диапазона, введите корректное число')
-               i -= 1
-            else:
-                self.list_of_numbers.append(n)
-        return self.list_of_numbers
+            while len(self.list_of_numbers) != self.total:
+                n = int(input('Введите число от 1 до 50: '))
+                if n > 50:
+                    print(f'{n} не в рамках диапазона, введите корректное число')
+                else:
+                    self.list_of_numbers.append(n)
+        print(self.list_of_numbers)
+        return self
 
+    def index(self, index_number: int) -> int:
+        index_number = self.list_of_numbers.index(index_number)
+        return index_number
+
+    #поиск четных
     def even(self) -> list:
-        for i in self.list_of_numbers:
-            if i % 2 != 0:
-               self.list_of_numbers.remove(i)
-        return self.list_of_numbers
+       #self.list_of_numbers = [x for x in self.list_of_numbers if x % 2 == 0]
+       for x in reversed(self.list_of_numbers):
+           if x % 2 != 0:
+               self.list_of_numbers.remove(x)
+       print(self.list_of_numbers)
+       return self
 
-
+    #поиск нечетных
     def odd(self) -> list:
-        for i in self.list_of_numbers:
+        for i in reversed(self.list_of_numbers):
             if i % 2 == 0:
                self.list_of_numbers.remove(i)
-        return self.list_of_numbers
+        print(self.list_of_numbers)
+        return self
 
-
+    #ищем, что идет до элемента
     def under(self, number: int) -> list:
-        index_number = self.list_of_numbers.index(number)
-        return self.list_of_numbers[:index_number]
+        self.list_of_numbers = self.list_of_numbers[:number]
+        print(self.list_of_numbers)
+        return self
 
+    #ищем, что идет после элемента
     def over(self, number: int) -> list:
-        index_number = self.list_of_numbers.index(number)
-        return self.list_of_numbers[index_number+1:]
+
+        self.list_of_numbers = self.list_of_numbers[number+1::]
+        print(self.list_of_numbers)
+
+        return self
 
     def in_range(self, start: int, stop: int) -> list:
         index_start = self.list_of_numbers.index(start)
         index_stop = self.list_of_numbers.index(stop)
-        return self.list_of_numbers[index_start:index_stop+1]
+        self.list_of_numbers = self.list_of_numbers[index_start:index_stop+1]
+        print(self.list_of_numbers)
+        return self
 
-dd = Filter(5)
-print(dd.add_numbers())
 
-#print(dd.even())
-#print(dd.odd())
-#print(dd.under(17))
-#print(dd.over(16))
-#print(dd.in_range(2, 14))
+
+object = Filter(5)
+
+object.add_numbers()
+#object.over(2).even()
+
+#в скобках ставлю индекс
+object.under(2).odd()
+
+
+
